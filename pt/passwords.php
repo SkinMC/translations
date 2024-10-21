b480f971b1ae52dbb63bbb3a34fce0b372dd7d15
Zane <62026364+zanethefox@users.noreply.github.com>
2024-10-22 01:49:30 +0200

    New translations passwords.php (Portuguese, Brazilian)

--- pt/passwords.php
+++ pt/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Sua senha foi alterada!',
+    'reset' => 'Sua senha foi redefinida!',
     'sent' => 'Enviamos um e-mail com o link para redefinir a senha!',
-    'throttled' => 'Por favor, espere antes de tentar novamente.',
-    'token' => 'Token para recuperação de senha inválido.',
+    'throttled' => 'Espere antes de tentar novamente.',
+    'token' => 'Token de redefinição de senha inválido.',
     'user' => "Não foi possível encontrar o usuário com este e-mail.",
 
 ];
