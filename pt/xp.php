4bcab748f690f87e9d0adc4030eae7f0f874e79c
Zane <62026364+zanethefox@users.noreply.github.com>
2024-10-22 02:51:05 +0200

    New translations xp.php (Portuguese, Brazilian)

--- pt/xp.php
+++ pt/xp.php
@@ -11,15 +11,15 @@ return [
     */
     'award' => [
         'feed_post' => 'Publicado no feed',
-        'tag_approved' => 'Sua Tag foi aprovada',
-        'tag_approved_vote' => 'A Tag votada foi aprovada',
+        'tag_approved' => 'A tag adicionada por você foi aprovada',
+        'tag_approved_vote' => 'A tag que você votou foi aprovada',
         'comment' => 'Deixou um comentário',
-        'comment_reply' => 'XP extra por responder um comentário',
-        'generic' => 'XP Ganho',
-        'daily_login' => 'XP de Login Diário',
-        'sign_up' => 'Bem-vindo ao SkinMC.',
-        'invite_friend' => 'Convide amigos',
+        'comment_reply' => 'XP adicional por responder um comentário',
+        'generic' => 'Obteve EXP',
+        'daily_login' => 'EXP de registro diário',
+        'sign_up' => 'Bem-vindo(a) ao SkinMC.',
+        'invite_friend' => 'Convidar amigo',
         'invited_friend' => 'Convidado por um amigo',
     ],
-    'empty' => 'Ganhe XP para ver aqui.',
+    'empty' => 'Obtenha mais EXP para ver aqui.',
 ];
