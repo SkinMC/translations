1089a2b84a5666753e97c2cd0ed333a757e49750
Zane <62026364+zanethefox@users.noreply.github.com>
2025-06-02 17:20:18 +0200

    New translations pages.php (German)

--- de/pages.php
+++ de/pages.php
@@ -697,6 +697,14 @@ return [
             'drag' => 'Ziehe einen Skin hierher oder klicke',
             'click' => 'Bild auswählen'
         ],
+        'download' => [
+            'error' => [
+                'no-data' => 'No skin data was provided. Please try again.',
+                'invalid-format' => 'The skin data format is invalid. Please try again.',
+                'invalid-data' => 'The skin data is corrupted. Please try again.',
+                'generic' => 'Something went wrong while downloading your skin. Please try again.',
+            ],
+        ],
         'draw' => 'Zeichnen',
         'eyedropper' => 'Farbpipette',
         'fill' => 'Füllen',
