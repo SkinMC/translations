dcb61ec745c80ba2d75d390c4f434dc0a179416d
Zane <62026364+zanethefox@users.noreply.github.com>
2025-06-09 03:50:55 +0200

    New translations passwords.php (Persian)

--- fa/passwords.php
+++ fa/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'گذرواژه شما بازگردانی شد!',
+    'sent' => 'لینک بازگردانی گذرواژه به ایمیل شما ارسال شد!',
+    'throttled' => 'لطفا قبل از تلاش مجدد صبر کنید.',
+    'token' => 'مشخصه‌ی بازگردانی گذرواژه معتبر نیست.',
+    'user' => "ما کاربری با این نشانی ایمیل نداریم.",
 
 ];
