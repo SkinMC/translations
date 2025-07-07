7399c69f89bf69c4a624da1d9099e05dd105cc16
Zane <62026364+zanethefox@users.noreply.github.com>
2025-07-07 15:52:25 +0200

    New translations cookies.php (Finnish)

--- vendor/eu-cookie-consent/fi/cookies.php
+++ vendor/eu-cookie-consent/fi/cookies.php
@@ -1,32 +1,32 @@
 <?php
 
 return [
-    'acceptAllButton' => 'Accept all',
-    'save' => 'Save',
-    'PopupTitle' => 'Cookie consent',
-    'PopupDescription' => 'SkinMC uses cookies to provide functionality and features.',
+    'acceptAllButton' => 'Hyväksy kaikki',
+    'save' => 'Tallenna',
+    'PopupTitle' => 'Evästeiden suostumus',
+    'PopupDescription' => 'SkinMC käyttää evästeitä toiminnallisuuden ja ominaisuuksien tarjoamiseen.',
 
-    'customise' => 'Customise',
-    'customise_title' => 'Customise cookies',
+    'customise' => 'Muokkaa',
+    'customise_title' => 'Muokkaa evästeitä',
 
-    'essential' => 'Essential data',
-    'essential_description' => 'Essential data cookies are required to run the site you are visiting technically. You can not deactivate them.',
+    'essential' => 'Välttämättömät tiedot',
+    'essential_description' => 'Välttämättömien tietojen evästeitä tarvitaan, jotta vierailusi kohteena oleva sivusto toimisi teknisesti. Et voi poistaa niitä käytöstä.',
 
-    'session' => 'Session Cookie: PHP uses a Cookie to identify user sessions.',
-    'xsrf-token' => 'XSRF-Token Cookie: automatically created CSRF "token" for each active user session managed by the application. This token is used to verify that the authenticated user is the one actually making the requests to the application.',
+    'session' => 'Istunnon eväste: PHP käyttää evästettä käyttäjäistuntojen tunnistamiseen.',
+    'xsrf-token' => 'XSRF-Token Eväste: automaattisesti luotu CSRF-”token” jokaiselle sovelluksen hallinnoimaa aktiivista käyttäjäistuntoa varten. Tätä tunnusta käytetään sen tarkistamiseen, että todennettu käyttäjä on se, joka todella tekee pyynnöt sovellukselle.',
 
-    'functional' => 'Functional cookies',
-    'functional_description' => 'These cookies allow the provision of enhanced functionality such as videos. They may be set by us or by third-party providers whose services we have added to our pages. If you do not allow these cookies, then some or all of these functionalities may not function properly.',
+    'functional' => 'Toiminnalliset evästeet',
+    'functional_description' => 'Nämä evästeet mahdollistavat parannettujen toimintojen, kuten videoiden, tarjoamisen. Me tai kolmannen osapuolen palveluntarjoajat, joiden palvelut olemme lisänneet sivuillemme, saattavat asettaa ne. Jos et salli näitä evästeitä, jotkin tai kaikki näistä toiminnoista eivät välttämättä toimi kunnolla.',
     'youtube' => 'YouTube',
 
-    'analytical' => 'Analytical cookies',
-    'analytical_description' => 'These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. If you do not allow these cookies, information from these cookies will not be used to help site performance.',
+    'analytical' => 'Analyyttiset evästeet',
+    'analytical_description' => 'Näiden evästeiden avulla voimme laskea käyntejä ja liikenteen lähteitä, jotta voimme mitata ja parantaa sivustomme suorituskykyä. Jos et salli näitä evästeitä, näiden evästeiden tietoja ei käytetä sivuston suorituskyvyn parantamiseen.',
     'googleAnalytics' => 'Google Analytics',
     'microsoftClarity' => 'Microsoft Clarity',
 
-    'googleOneTap' => 'Sign in with Google',
-    'googleOneTap_description' => 'These cookies allow functionality from Sign in with Google to quickly sign in or create an account with Google. Data from Sign in with Google is only used for security purposes.',
+    'googleOneTap' => 'Kirjaudu sisään Googlella',
+    'googleOneTap_description' => 'Nämä evästeet sallivat Googlen sisäänkirjautumisen ja tilin nopean kirjautumisen Googlen kautta. Googlen kautta kirjautuvia tietoja käytetään vain tietoturvatarkoituksiin.',
 
-    'facebookLogin' => 'Sign in with Facebook',
-    'facebookLogin_description' => 'These cookies allow functionality from Sign in with Facebook to quickly sign in or create an account with Facebook.',
+    'facebookLogin' => 'Kirjaudu Facebookilla',
+    'facebookLogin_description' => 'Nämä evästeet sallivat Facebookilla kirjautumisen toiminnallisuuden kirjautua sisään tai luoda tilin Facebookilla.',
 ];
