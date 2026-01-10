820e61197cbbc34d8e78e45a493d3687a7f3df77
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-11 00:03:33 +0100

    New translations navigation.php (Swedish)

--- sv/navigation.php
+++ sv/navigation.php
@@ -10,9 +10,17 @@ return [
     'achievement' => 'Framsteg',
     'armor' => 'Rustningsställ',
     'banners' => 'Banderoller',
+    'banner' => 'Banner Editor',
     'profile' => 'Min profil',
 
     'generators' => 'Generatorer',
+
+    // Navigation dropdowns
+    'feed' => 'Feed',
+    'discover' => 'Discover',
+    'mods' => 'Mods',
+    'shaders' => 'Shaders',
+    'resource_packs' => 'Resource Packs',
 
     'logout' => 'Logga ut',
     'register' => 'Registrera dig',
@@ -58,6 +66,8 @@ return [
     'app' => 'App',
     'mod' => 'Mod',
     'totem' => 'Totem',
+    'cube' => 'Cube',
+    'new' => 'New',
 
     'feedback' => 'Feedback',
     'status' => 'Status',
@@ -68,8 +78,9 @@ return [
     'add' => 'Lägg till',
 
     'theme' => [
+        'title' => 'Theme',
         'dark' => 'Mörk',
         'light' => 'Ljus',
-        'auto' => 'Automatisk'
+        'auto' => 'Auto'
     ],
 ];
