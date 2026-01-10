f9147c62cc22ccc8174f53a41cc7b7b6bb9f98ab
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-11 00:03:28 +0100

    New translations navigation.php (Japanese)

--- ja/navigation.php
+++ ja/navigation.php
@@ -10,9 +10,17 @@ return [
     'achievement' => '実績',
     'armor' => '防具立て',
     'banners' => 'バナー',
+    'banner' => 'Banner Editor',
     'profile' => 'マイプロフィール',
 
     'generators' => '生成',
+
+    // Navigation dropdowns
+    'feed' => 'Feed',
+    'discover' => 'Discover',
+    'mods' => 'Mods',
+    'shaders' => 'Shaders',
+    'resource_packs' => 'Resource Packs',
 
     'logout' => 'ログアウト',
     'register' => 'サインアップ',
@@ -58,6 +66,8 @@ return [
     'app' => 'アプリ',
     'mod' => 'Mod',
     'totem' => 'トーテム',
+    'cube' => 'Cube',
+    'new' => 'New',
 
     'feedback' => 'フィードバック',
     'status' => 'ステータス',
@@ -68,8 +78,9 @@ return [
     'add' => '追加',
 
     'theme' => [
+        'title' => 'Theme',
         'dark' => 'ダーク',
         'light' => 'ライト',
-        'auto' => 'オートマチック'
+        'auto' => 'Auto'
     ],
 ];
