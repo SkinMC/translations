87c0a632d8936e161da80c1531b31fe582831678
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-11 21:34:05 +0100

    New translations navigation.php (Ukrainian)

--- uk/navigation.php
+++ uk/navigation.php
@@ -21,6 +21,9 @@ return [
     'mods' => 'Mods',
     'shaders' => 'Shaders',
     'resource_packs' => 'Resource Packs',
+    'modpacks' => 'Modpacks',
+    'datapacks' => 'Data Packs',
+    'plugins' => 'Plugins',
 
     'logout' => 'Вийти',
     'register' => 'Зареєструватися',
