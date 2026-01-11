962fb78ae48fffc96a290b2538c8e88cb8306c93
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-11 21:33:50 +0100

    New translations navigation.php (Spanish)

--- es/navigation.php
+++ es/navigation.php
@@ -21,6 +21,9 @@ return [
     'mods' => 'Mods',
     'shaders' => 'Shaders',
     'resource_packs' => 'Resource Packs',
+    'modpacks' => 'Modpacks',
+    'datapacks' => 'Data Packs',
+    'plugins' => 'Plugins',
 
     'logout' => 'Cerrar sesión',
     'register' => 'Registrarse',
