b461d9478ddf77973796f150342eac164e9fb587
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-16 11:53:57 +0100

    New translations pages.php (Turkish)

--- tr/pages.php
+++ tr/pages.php
@@ -774,6 +774,12 @@ return [
             'decrease_brightness' => 'Şu anda aktif olan rengin parlaklığını azaltın.',
             'increase_brightness' => 'Şu anda aktif olan rengin parlaklığını artırın.',
             'erase' => 'İkincil kaplama katmanına çizilen pikselleri silin.',
+            'minimap' => 'Minimap',
+            'minimap_info' => 'The minimap can show you the skin texture in a flat view while in 3D mode. In 2D mode the minimap switches to a 3D preview of your skin. You can zoom in and out.',
+            'autosave' => 'Autosave',
+            'autosave_info' => 'The autosave feature saves your skin automatically every 30 seconds. You can access the autosaved skins in the editor by pressing the folder icon, and choosing "Previous skins".',
+            '2d_editor' => '2D and 3D editor',
+            '2d_editor_info' => 'Toggle between editing your Minecraft skin in 2D or 3D mode.',
         ],
     ],
     'account' => [
