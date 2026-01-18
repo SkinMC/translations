626f5c2f3ef9ab987a0f2f9c436c33b049f7a6ad
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-18 11:32:07 +0100

    New translations socialite.php (Hungarian)

--- hu/socialite.php
+++ hu/socialite.php
@@ -13,9 +13,11 @@ return [
     'link' => [
         'error' => [
             'missing-game-copy' => 'Ez a Microsoft-fiók nem rendelkezik a Minecrafttal. Látogasd meg a minecraft.net-et és jelentkezz be azzal a Microsoft-fiókkal, ami alatt megvetted a Minecraft Java-kiadást.',
-            'generic' => 'Hmm... something went wrong. Please try again or contact SkinMC support via e-mail or Discord for assistance.'
+            'generic' => 'Hmm... something went wrong. Please try again or contact SkinMC support via e-mail or Discord for assistance.',
+            'already-linked' => 'Minecraft account is already linked',
+            'profile-already-linked' => 'This Minecraft account is already linked to another user',
+            'failed' => 'Failed to link Minecraft account. Please try again.',
         ],
     ],
 
 ];
-
