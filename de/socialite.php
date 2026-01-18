e328c474412de3ba012ce935900d04e0d0b9878c
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-18 11:32:04 +0100

    New translations socialite.php (German)

--- de/socialite.php
+++ de/socialite.php
@@ -13,9 +13,11 @@ return [
     'link' => [
         'error' => [
             'missing-game-copy' => 'Dieses Microsoft-Konto besitzt kein Minecraft. Gehe zu Minecraft.net und melde dich mit deinem Microsoft-Konto an, das Minecraft Java Edition besitzt.',
-            'generic' => 'Hmm... Etwas ist schief gelaufen. Bitte versuche es erneut oder kontaktiere den SkinMC Support per E-Mail oder Discord um Hilfe zu erhalten.'
+            'generic' => 'Hmm... Etwas ist schief gelaufen. Bitte versuche es erneut oder kontaktiere den SkinMC Support per E-Mail oder Discord um Hilfe zu erhalten.',
+            'already-linked' => 'Minecraft account is already linked',
+            'profile-already-linked' => 'This Minecraft account is already linked to another user',
+            'failed' => 'Failed to link Minecraft account. Please try again.',
         ],
     ],
 
 ];
-
