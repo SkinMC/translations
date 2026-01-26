78aa46c04ab008eece93a4c4b19573023d43ce79
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-26 15:04:53 +0100

    New translations navigation.php (Swedish)

--- sv/navigation.php
+++ sv/navigation.php
@@ -81,6 +81,16 @@ return [
     'listings' => 'Listor',
     'add' => 'Lägg till',
 
+    // Autocomplete types
+    'autocomplete' => [
+        'profile' => 'Profile',
+        'server' => 'Server',
+        'project' => 'Project',
+        'skin_tag' => 'Skin Tag',
+        'server_category' => 'Server Category',
+        'modrinth_category' => 'Project Category',
+    ],
+
     'theme' => [
         'title' => 'Theme',
         'dark' => 'Mörk',
