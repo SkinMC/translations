652deda575f725630430236218e3e00b84f3b4ac
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-26 15:04:35 +0100

    New translations navigation.php (French)

--- fr/navigation.php
+++ fr/navigation.php
@@ -81,6 +81,16 @@ return [
     'listings' => 'Listes',
     'add' => 'Ajouter',
 
+    // Autocomplete types
+    'autocomplete' => [
+        'profile' => 'Profile',
+        'server' => 'Server',
+        'project' => 'Project',
+        'skin_tag' => 'Skin Tag',
+        'server_category' => 'Server Category',
+        'modrinth_category' => 'Project Category',
+    ],
+
     'theme' => [
         'title' => 'Thème',
         'dark' => 'Sombre',
