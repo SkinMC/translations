fb3ed44b047f6d74a42c5b072097e33edcbeaf00
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-26 15:04:37 +0100

    New translations navigation.php (Arabic)

--- ar/navigation.php
+++ ar/navigation.php
@@ -81,6 +81,16 @@ return [
     'listings' => 'القوائم',
     'add' => 'إضافة',
 
+    // Autocomplete types
+    'autocomplete' => [
+        'profile' => 'Profile',
+        'server' => 'Server',
+        'project' => 'Project',
+        'skin_tag' => 'Skin Tag',
+        'server_category' => 'Server Category',
+        'modrinth_category' => 'Project Category',
+    ],
+
     'theme' => [
         'title' => 'Theme',
         'dark' => 'داكن',
