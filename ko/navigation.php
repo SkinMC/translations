b8d838e1590663a35626090de1d7ee041eaf7ae0
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-26 15:04:45 +0100

    New translations navigation.php (Korean)

--- ko/navigation.php
+++ ko/navigation.php
@@ -81,6 +81,16 @@ return [
     'listings' => '목록',
     'add' => '추가',
 
+    // Autocomplete types
+    'autocomplete' => [
+        'profile' => 'Profile',
+        'server' => 'Server',
+        'project' => 'Project',
+        'skin_tag' => 'Skin Tag',
+        'server_category' => 'Server Category',
+        'modrinth_category' => 'Project Category',
+    ],
+
     'theme' => [
         'title' => 'Theme',
         'dark' => '어둡게',
