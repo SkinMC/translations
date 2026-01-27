52d77a2b787511cd967f829f5b12c576902bbc1e
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-27 23:31:15 +0100

    New translations socialite.php (Russian)

--- ru/socialite.php
+++ ru/socialite.php
@@ -14,9 +14,9 @@ return [
         'error' => [
             'missing-game-copy' => 'К этой учётной записи не привязан профиль Minecraft. Перейдите на Minecraft.net и войдите с учётной записью к которой привязан Minecraft Java Edition.',
             'generic' => 'Что-то пошло не так... Попробуйте ещё раз или проконсультируйтесь с технической поддержкой SkinMC по электронной почте или через Discord.',
-            'already-linked' => 'Minecraft account is already linked',
-            'profile-already-linked' => 'This Minecraft account is already linked to another user',
-            'failed' => 'Failed to link Minecraft account. Please try again.',
+            'already-linked' => 'Аккаунт Minecraft уже привязан',
+            'profile-already-linked' => 'Эта учетная запись Minecraft уже привязана к другому пользователю',
+            'failed' => 'Не удалось подключить учетную запись Minecraft. Повторите попытку.',
         ],
     ],
 
