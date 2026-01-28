64549c3ef0c8045c4b16c62f6554ef60084c4f1c
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-28 13:23:08 +0100

    New translations pages.php (Hungarian)

--- hu/pages.php
+++ hu/pages.php
@@ -49,6 +49,13 @@ return [
         'achievement' => 'Teljesítményszerkesztő',
         'achievement_detail' => 'Készíts egyedi teljesítményeket!',
         'continue' => 'Böngészés folytatása',
+        'totem' => 'Minecraft Totem Generator',
+        'totem_description' => 'Create a custom Minecraft resource pack with a Totem of Undying from any skin.',
+        'banner' => 'Minecraft Banner Generator',
+        'banner_description' => 'Generate and preview custom Minecraft banners and patterns.',
+        'capes' => 'Minecraft Capes',
+        'capes_description' => 'Browse the largest collection of Minecraft capes, or create your own.',
+
     ],
     'profile' => [
         'verified' => 'Igazolt fiók',
