7870242acedbf13845b49ee8d756bd163ed81351
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-28 13:23:21 +0100

    New translations pages.php (Swedish)

--- sv/pages.php
+++ sv/pages.php
@@ -49,6 +49,13 @@ return [
         'achievement' => 'Achievement generator',
         'achievement_detail' => 'Create your own, custom and personalised achievements.',
         'continue' => 'Continue browsing',
+        'totem' => 'Minecraft Totem Generator',
+        'totem_description' => 'Create a custom Minecraft resource pack with a Totem of Undying from any skin.',
+        'banner' => 'Minecraft Banner Generator',
+        'banner_description' => 'Generate and preview custom Minecraft banners and patterns.',
+        'capes' => 'Minecraft Capes',
+        'capes_description' => 'Browse the largest collection of Minecraft capes, or create your own.',
+
     ],
     'profile' => [
         'verified' => 'Verifierad profil',
