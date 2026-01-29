f4d0af1578ce088dfd35cf11372a1c496c613de5
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-29 23:33:56 +0100

    New translations socialite.php (Finnish)

--- fi/socialite.php
+++ fi/socialite.php
@@ -14,9 +14,9 @@ return [
         'error' => [
             'missing-game-copy' => 'Tällä Microsoft-tilillä ei ole Minecraftia. Siirry osoitteeseen Minecraft.net ja kirjaudu sisään siihen Microsoft-tiliin, jolla on Minecraft Java Edition.',
             'generic' => 'Hmm... jokin meni pieleen. Yritä uudelleen tai ota yhteyttä SkinMC tukeen sähköpostitse tai Discordissa saadaksesi apua.',
-            'already-linked' => 'Minecraft account is already linked',
-            'profile-already-linked' => 'This Minecraft account is already linked to another user',
-            'failed' => 'Failed to link Minecraft account. Please try again.',
+            'already-linked' => 'Minecraft-tili on jo linkitetty',
+            'profile-already-linked' => 'Tämä Minecraft-tili on jo linkitetty toiseen käyttäjään',
+            'failed' => 'Minecraft-tilin yhdistäminen epäonnistui. Yritä uudelleen.',
         ],
     ],
 
