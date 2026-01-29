febf25abcb8685ea651471a6388a1db02247fdb8
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-29 23:33:57 +0100

    New translations cookies.php (Finnish)

--- vendor/eu-cookie-consent/fi/cookies.php
+++ vendor/eu-cookie-consent/fi/cookies.php
@@ -2,7 +2,7 @@
 
 return [
     'acceptAllButton' => 'Hyväksy kaikki',
-    'declineAll' => 'Decline All',
+    'declineAll' => 'Hylkää Kaikki',
     'save' => 'Tallenna',
     'PopupTitle' => 'Evästeiden suostumus',
     'PopupDescription' => 'SkinMC käyttää evästeitä toiminnallisuuden ja ominaisuuksien tarjoamiseen.',
