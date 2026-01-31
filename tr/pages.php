40c9a15c840525cc9402baf2f04c586b37b7a470
Zane <62026364+zanethefox@users.noreply.github.com>
2026-01-31 16:01:18 +0100

    New translations pages.php (Turkish)

--- tr/pages.php
+++ tr/pages.php
@@ -576,6 +576,7 @@ return [
         'toast' => [
             'added' => 'Favoriler listenize eklendi',
             'removed' => 'Favoriler listenizden çıkarıldı',
+            'login_required' => 'Sign in to add to favourites',
         ],
         'skins' => [
             'button' => 'Skinler',
