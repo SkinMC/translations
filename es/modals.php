0a42d8a3a2a73757742c92ee4f71b3109efbe328
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-01 05:10:28 +0100

    New translations modals.php (Spanish)

--- es/modals.php
+++ es/modals.php
@@ -1,40 +1,69 @@
 <?php
 
 return [
-
     'login' => [
         'title' => 'Iniciar sesión',
-        'email' => 'Dirección de email',
+        'email' => 'Correo electrónico',
         'password' => 'Contraseña',
         'button' => 'Iniciar sesión',
-        'forgot' => '¿Olvidaste tu contraseña?',
-        'reset' => 'Reset',
-        'register' => '¿Aún no tienes una cuenta?',
-        'signup' => 'Regístrate',
+        'forgot' => '¿Olvidó su contraseña?',
+        'reset' => 'Restablecer',
+        'register' => '¿Aún no tiene una cuenta?',
+        'signup' => 'Registrarse',
         'google' => 'Continuar con Google',
-        'facebook' => 'Continuar con Facebook',
-        'twitter' => 'Continuar con Twitter',
-        'divider' => 'O',
+        'microsoft' => 'Continuar con Microsoft',
+        'facebook' => 'Conectar con Facebook',
+        'twitter' => 'Iniciar sesión con Twitter',
+        'divider' => 'o',
     ],
     'signup' => [
-        'title' => 'Regístrate',
+        'title' => 'Registrarse',
         'existing' => '¿Ya tienes una cuenta?',
-        'button' => 'Regístrate',
+        'button' => 'Aceptar y continuar',
+        'terms_by_selecting' => 'Al seleccionar',
+        'terms_agree_to' => ', I agree to SkinMC\'s',
+        'terms_acknowledge' => 'y reconozco el',
     ],
     'recover' => [
         'title' => 'Recuperar cuenta',
-        'description' => '¿Has perdido tu contraseña? Recuperala introduciendo tu dirección de correo electrónico para recuperarla.',
-        'button' => 'Enviar email de recuperación',
-        'found' => '¿Has encontrado tu contraseña?',
+        'description' => '¿Has perdido tu contraseña? Recupérala introduciendo tu dirección de correo electrónico para recuperarla.',
+        'button' => 'Enviar un correo de recuperación',
+        'found' => '¿Recordó su contraseña?',
+    ],
+    'logout' => [
+        'title' => 'Cerrar sesión',
+        'description' => '¿Estás seguro de que quieres cerrar sesión en tu cuenta de SkinMC?',
+        'logout' => 'Cerrar sesión',
+        'cancel' => 'Cancelar',
     ],
     'profile_qrcode' => [
-        'title' => 'Scan QR code',
-        'done' => 'Hecho',
+        'title' => 'Escanear código QR',
+        'done' => 'Listo',
     ],
     'language' => [
         'title' => 'Idioma',
-        'description' => 'Elige tu idioma',
+        'description' => 'Seleccione su idioma',
         'contribute' => 'Contribuir',
-        'contribute_add' => 'Añadir traducción',
+        'contribute_add' => 'Agregar traducción',
+        'updated' => 'Se ha establecido tu idioma'
+    ],
+    'copied' => '¡Copiado al portapapeles!',
+    'close' => 'Cerrar',
+    'feedback' => [
+        'title' => 'Sugerencias',
+        'type' => [
+            'select' => 'Seleccionar tipo de sugerencia',
+            'label' => 'Tipo de sugerencia',
+            'comment' => 'Comentarios',
+            'question' => 'Preguntas',
+            'bug' => 'Reportar error',
+            'feature' => 'Solicitar función',
+            ],
+        'submit' => 'Enviar',
+        'nps' => 'En una escala de 0 a 10, ¿Qué tan probable es que recomiendes SkinMC a un amigo?',
+        'success' => [
+            'title' => '¡Gracias!',
+            'message' => 'Tu mensaje ha sido enviado, ¡Espera una respuesta pronto! Para más preguntas, siéntete libre de unirte al Discord o enviar otro mensaje.',
+        ]
     ]
 ];
