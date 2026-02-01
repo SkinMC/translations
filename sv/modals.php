6e3057b4769fecde8f68544cc761773c57c897b7
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-01 05:10:33 +0100

    New translations modals.php (Swedish)

--- sv/modals.php
+++ sv/modals.php
@@ -1,40 +1,69 @@
 <?php
 
 return [
-
     'login' => [
         'title' => 'Logga in',
         'email' => 'E-postadress',
         'password' => 'Lösenord',
         'button' => 'Logga in',
         'forgot' => 'Glömt ditt lösenord?',
-        'reset' => 'Reset',
-        'register' => 'Har du inget konto än?',
+        'reset' => 'Återställ',
+        'register' => 'Har du inte ett konto än?',
         'signup' => 'Registrera dig',
         'google' => 'Fortsätt med Google',
+        'microsoft' => 'Fortsätt med Microsoft',
         'facebook' => 'Fortsätt med Facebook',
         'twitter' => 'Fortsätt med Twitter',
-        'divider' => 'ELLER',
+        'divider' => 'eller',
     ],
     'signup' => [
         'title' => 'Registrera dig',
         'existing' => 'Har du redan ett konto?',
-        'button' => 'Registrera dig',
+        'button' => 'Godkänn och fortsätt',
+        'terms_by_selecting' => 'Genom att välja',
+        'terms_agree_to' => ', jag godkänner SkinMCs',
+        'terms_acknowledge' => 'och bekräftar',
     ],
     'recover' => [
         'title' => 'Återställ konto',
-        'description' => 'Glömt ditt lösenord? Återskapa det genom att ange din e-postadress för att få det tillbaka.',
-        'button' => 'Skicka återställningsmail',
-        'found' => 'Hittade du ditt lösenord?',
+        'description' => 'Glömt ditt lösenord? Återställ det genom att ange in din e-postadress för att få tillbaka det.',
+        'button' => 'Skicka återställningsmejl',
+        'found' => 'Hittat ditt lösenord?',
+    ],
+    'logout' => [
+        'title' => 'Logga ut',
+        'description' => 'Är du säker på att du villa logga ut från ditt SkinMC konto?',
+        'logout' => 'Logga ut',
+        'cancel' => 'Avbryt',
     ],
     'profile_qrcode' => [
-        'title' => 'Scan QR code',
+        'title' => 'Skanna QR-kod',
         'done' => 'Klar',
     ],
     'language' => [
         'title' => 'Språk',
-        'description' => 'Välj ditt språk',
+        'description' => 'Välj ett språk',
         'contribute' => 'Bidra',
         'contribute_add' => 'Lägg till översättning',
+        'updated' => 'Ditt språk har ändrats'
+    ],
+    'copied' => 'Kopierade till urklipp!',
+    'close' => 'Stäng',
+    'feedback' => [
+        'title' => 'Feedback',
+        'type' => [
+            'select' => 'Välj feedback typ',
+            'label' => 'Feedback typ',
+            'comment' => 'Kommentarer',
+            'question' => 'Frågor',
+            'bug' => 'Buggrapport',
+            'feature' => 'Funktionsförfrågan',
+            ],
+        'submit' => 'Skicka',
+        'nps' => 'På en skala från 0 till 10, hur troligt skulle du rekommendera SkinMC till en vän?',
+        'success' => [
+            'title' => 'Tack!',
+            'message' => 'Ditt meddelande har skickats och förvänta dig ett svar snart! För ytterligare frågor kan du gå med i våran Discord-server eller skicka ett annat meddelande.',
+        ]
     ]
 ];
