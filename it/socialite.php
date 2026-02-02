84c18870f846f2dfe5d633da34c01e56ae1d2494
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-02 01:29:36 +0100

    New translations socialite.php (Italian)

--- it/socialite.php
+++ it/socialite.php
@@ -14,9 +14,9 @@ return [
         'error' => [
             'missing-game-copy' => 'Questo account Microsoft non possiede Minecraft. Vai su Minecraft.net e accedi al tuo account Microsoft con cui ha comprato Minecraft Java Edition.',
             'generic' => 'Mhh... qualcosa è andato storto. Riprova o contatta il supporto SkinMC via e-mail o Discord per assistenza.',
-            'already-linked' => 'Minecraft account is already linked',
-            'profile-already-linked' => 'This Minecraft account is already linked to another user',
-            'failed' => 'Failed to link Minecraft account. Please try again.',
+            'already-linked' => 'L\'account Minecraft è già collegato',
+            'profile-already-linked' => 'Questo account Minecraft è già collegato ad un altro utente',
+            'failed' => 'Impossibile collegare l\'account Minecraft. Per favore riprova.',
         ],
     ],
 
