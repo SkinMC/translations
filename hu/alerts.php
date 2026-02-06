3adae83310721ffb5165c5f9325a275939509955
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-06 12:35:05 +0100

    New translations alerts.php (Hungarian)

--- hu/alerts.php
+++ hu/alerts.php
@@ -11,25 +11,25 @@ return [
     */
     'follower' => [
         'title' => 'Új követő',
-        'text' => ':user követett Téged!'
+        'text' => ':user követ téged!'
     ],
     'comment' => [
         'new' => [
             'title' => 'Hozzászólás ehhez: :model',
-            'text' => ':user hozzászólt ehhez: :model'
+            'text' => ':user hozzászólt a :model -led hez.'
         ],
         'reply' => [
             'title' => 'Új válasz ennél: :model',
             'text' => ':user válaszolt a hozzászólásodra ezen: :model'
         ],
         'like' => [
-            'title' => 'Comment liked',
-            'text' => ':user liked your comment!'
+            'title' => 'Hozzászólás kedvelve',
+            'text' => ':user: kedveli a hozzászólásod!'
         ]
     ],
     'post' => [
         'like' => [
-            'title' => 'Új kedvelés',
+            'title' => 'Kedvelést szereztél',
             'text' => ':user kedvelte a bejegyzésedet!',
         ],
         'mention' => [
@@ -37,28 +37,28 @@ return [
             'text' => ':user megemlített Téged!'
         ],
         'repost' => [
-            'title' => 'Repost',
-            'text' => ':user reposted your post!'
+            'title' => 'Újraposztolás',
+            'text' => ':user: újraposztolta a posztod!'
         ]
     ],
     'level' => [
         'up' => [
             'title' => 'Szintet léptél!',
-            'text' => 'Congratulations on reaching level :level!'
+            'text' => 'Gratulálunk a(z) :level:. szint eléréséhez!'
         ]
     ],
     'report' => [
         'created' => [
-            'title' => 'We\'ve received your report! (Case :id)',
-            'text' => 'Our team is on it and will look into it right away. Thanks for being awesome and helping us keep things chill! '
+            'title' => 'Hozzánk került a bejelentésed! (Ügy :id)',
+            'text' => 'A csapatunk dolgozik rajta, és azonnal vet egy pillantást a problémádra. Köszönjük hogy ilyen király és fantasztikus vagy hogy segítesz nekünk nyugisan tartani a dolgokat! '
         ],
         'resolved' => [
             'title' => 'Megnéztük a jelentésedet (Ügy :id)',
-            'text' => 'Hi there, the reported issue has been taken care of and is now sorted. Thanks for being a superhero of our community!',
+            'text' => 'Üdv, a bejelentett problémáról már gondoskodtunk, és szortíroztuk. Köszönjük hogy a szuperhőse vagy a közösségünknek!',
         ],
         'dismissed' => [
             'title' => 'Az ügyed frissítve lett (Ügy :id)',
-            'text' => 'After some detective work, we\'ve found that the content you had reported doesn\'t go against our guidelines. If you want us to review something specific, please make sure that you report the skin, profile, etc, and provide additional context when needed. Thanks for your concern and involvement in our community!',
+            'text' => 'Egy kis kutatómunka után megtaláltuk hogy a probléma amit bejelentettél nem megy szembe a szabályainkkal. Ha szeretnéd hogy valami specifikusba nézzünk bele, kérlek bizonyosodj meg róla hogy a skin-t, profilt, stb. amikor bejelented, írj mellé részletesebb információt ha kell. Köszönjük hogy érdekel hogy mi történik a közösségünkben!',
         ]
     ]
 ];
