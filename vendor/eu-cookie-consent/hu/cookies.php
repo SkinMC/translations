3c0d2a45c890cb0c453fac23ec1922494f19034d
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-06 12:35:02 +0100

    New translations cookies.php (Hungarian)

--- vendor/eu-cookie-consent/hu/cookies.php
+++ vendor/eu-cookie-consent/hu/cookies.php
@@ -2,25 +2,25 @@
 
 return [
     'acceptAllButton' => 'Összes elfogadása',
-    'declineAll' => 'Decline All',
+    'declineAll' => 'Minden elutasítása',
     'save' => 'Mentés',
-    'PopupTitle' => 'Cookie consent',
-    'PopupDescription' => 'SkinMC uses cookies to provide functionality and features.',
+    'PopupTitle' => 'Süti hozzájárulás',
+    'PopupDescription' => 'A SkinMC sütiket használ arra, hogy a legjobb élményt nyújtsa neked.',
 
-    'customise' => 'Customise',
-    'customise_title' => 'Customise cookies',
+    'customise' => 'Testreszabás',
+    'customise_title' => 'Sütik testreszabása',
 
-    'essential' => 'Essential data',
-    'essential_description' => 'Essential data cookies are required to run the site you are visiting technically. You can not deactivate them.',
+    'essential' => 'Alapvető adat',
+    'essential_description' => 'Az Alapvető adat sütik szükségesek az oldal tervezett működésének fenntartásához, ezeket nem tudod kikapcsolni.',
 
-    'session' => 'Session Cookie: PHP uses a Cookie to identify user sessions.',
-    'xsrf-token' => 'XSRF-Token Cookie: automatically created CSRF "token" for each active user session managed by the application. This token is used to verify that the authenticated user is the one actually making the requests to the application.',
+    'session' => 'Munkamenet süti: A PHP sütiket használ a felhasználó munkamenetének azonosításához.',
+    'xsrf-token' => 'XSRF-Token Süti: Automatikusan létrehozott CSRF "token" minden egyes aktív felhasználó munkamenetéhez, amit az applikáció kezel. Ez a token szükséges ahhoz hogy a rendszer igazolhassa, hogy a bejelentkezett felhasználó tényleg az, aki a hívásokat küldi az applikációnak.',
 
-    'functional' => 'Functional cookies',
-    'functional_description' => 'These cookies allow the provision of enhanced functionality such as videos. They may be set by us or by third-party providers whose services we have added to our pages. If you do not allow these cookies, then some or all of these functionalities may not function properly.',
+    'functional' => 'Funkcionális Sütik',
+    'functional_description' => 'Ezek a sütik lehetővé teszik a továbbfejlesztett funkciók biztosítását, például a videók megjelenítését. Beállíthatjuk őket mi magunk, vagy harmadik fél szolgáltatók, amelyek szolgáltatásait hozzáadtuk az oldalainkhoz. Ha nem engedélyezed ezeket a sütiket, akkor ezen funkciók egy része vagy egésze nem fog megfelelően működni.',
     'youtube' => 'YouTube',
 
-    'analytical' => 'Analytical cookies',
+    'analytical' => 'Elemző sütik',
     'analytical_description' => 'Ezek a sütik lehetővé teszik nekünk hogy számon tartsuk a weboldal látogatóinak számát, és azt hogy azok honnan jönnek, hogy a weboldal teljesítményét javíthassuk. Ha nem engedélyezed ezeket a sütiket, az információ amiket kapnánk belőle nem lesznek használhatóak a weboldal teljesítményének javítására.',
     'googleAnalytics' => 'Google Analytics',
     'microsoftClarity' => 'Microsoft Clarity',
