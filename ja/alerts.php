da602147cbebced0b698891ea493ed5bc92f3116
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-08 15:59:40 +0100

    New translations alerts.php (Japanese)

--- ja/alerts.php
+++ ja/alerts.php
@@ -11,21 +11,21 @@ return [
     */
     'follower' => [
         'title' => '新たなフォロワー',
-        'text' => ':userさんがあなたをフォローされました！'
+        'text' => ':userさんがあなたをフォローされました！',
     ],
     'comment' => [
         'new' => [
             'title' => ':model コメント',
-            'text' => ':userさんが:modelにコメントしました'
+            'text' => ':userさんが:modelにコメントしました',
         ],
         'reply' => [
             'title' => ':modelのコメントへの返信',
-            'text' => ':userさんが:modelコメントに返信しました'
+            'text' => ':userさんが:modelコメントに返信しました',
         ],
         'like' => [
             'title' => 'Comment liked',
-            'text' => ':user liked your comment!'
-        ]
+            'text' => ':user liked your comment!',
+        ],
     ],
     'post' => [
         'like' => [
@@ -34,23 +34,29 @@ return [
         ],
         'mention' => [
             'title' => '新たな返信',
-            'text' => ':userさんがあなたについて投稿しました！'
+            'text' => ':userさんがあなたについて投稿しました！',
         ],
         'repost' => [
             'title' => 'Repost',
-            'text' => ':user reposted your post!'
-        ]
+            'text' => ':user reposted your post!',
+        ],
     ],
     'level' => [
         'up' => [
             'title' => 'レベルアップ！',
-            'text' => 'おめでとうございます :levelに到達しました！'
-        ]
+            'text' => 'おめでとうございます :levelに到達しました！',
+        ],
+    ],
+    'board' => [
+        'collaborator_added' => [
+            'title' => 'Added to a board',
+            'text' => ':user added you as a collaborator to the board ":board".',
+        ],
     ],
     'report' => [
         'created' => [
             'title' => 'あなたのレポートをレビューしました! (ケース:id)',
-            'text' => '私たちがすぐに調査します。物事を冷静に保つのを協力してくれてありがとう！ '
+            'text' => '私たちがすぐに調査します。物事を冷静に保つのを協力してくれてありがとう！ ',
         ],
         'resolved' => [
             'title' => 'あなたのレポートをレビューしました (ケース:id)',
@@ -59,6 +65,6 @@ return [
         'dismissed' => [
             'title' => 'レポートの更新 (ケース:id)',
             'text' => 'いくつかの調査の結果、ご報告いただいた内容はガイドラインに反していないことがわかりました。具体的な内容を確認してほしい場合は、必ずスキンやプロフィールなどを報告してください。私たちのコミュニティへの関心と参加に感謝します！',
-        ]
-    ]
+        ],
+    ],
 ];
