0974c7f8d9241f6e13807750a2ac135abe01aab8
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-08 15:59:51 +0100

    New translations alerts.php (Swedish)

--- sv/alerts.php
+++ sv/alerts.php
@@ -11,21 +11,21 @@ return [
     */
     'follower' => [
         'title' => 'Ny följare',
-        'text' => ':user har börjat följa dig!'
+        'text' => ':user har börjat följa dig!',
     ],
     'comment' => [
         'new' => [
             'title' => ':model kommentar',
-            'text' => ':user kommenterade på din :model'
+            'text' => ':user kommenterade på din :model',
         ],
         'reply' => [
             'title' => 'Svara på :model kommentar',
-            'text' => ':user svarade på en :model kommentar'
+            'text' => ':user svarade på en :model kommentar',
         ],
         'like' => [
             'title' => 'Comment liked',
-            'text' => ':user liked your comment!'
-        ]
+            'text' => ':user liked your comment!',
+        ],
     ],
     'post' => [
         'like' => [
@@ -34,23 +34,29 @@ return [
         ],
         'mention' => [
             'title' => 'Ny omnämning',
-            'text' => ':user nämnde dig i deras inlägg!'
+            'text' => ':user nämnde dig i deras inlägg!',
         ],
         'repost' => [
             'title' => 'Repost',
-            'text' => ':user reposted your post!'
-        ]
+            'text' => ':user reposted your post!',
+        ],
     ],
     'level' => [
         'up' => [
             'title' => 'Du levlade upp!',
-            'text' => 'Grattis, du har nått level :level!'
-        ]
+            'text' => 'Grattis, du har nått level :level!',
+        ],
+    ],
+    'board' => [
+        'collaborator_added' => [
+            'title' => 'Added to a board',
+            'text' => ':user added you as a collaborator to the board ":board".',
+        ],
     ],
     'report' => [
         'created' => [
             'title' => 'Vi har fått din anmälan! (Id: :id)',
-            'text' => 'Vårat team är på det och kommer kolla på det på direkten. Tack för att du är fantastisk och hjälper oss att hålla det lugnt!'
+            'text' => 'Vårat team är på det och kommer kolla på det på direkten. Tack för att du är fantastisk och hjälper oss att hålla det lugnt!',
         ],
         'resolved' => [
             'title' => 'Vi har granskat din anmälan (Id: :id)',
@@ -59,6 +65,6 @@ return [
         'dismissed' => [
             'title' => 'Uppdatering till din rapport (Id: :id)',
             'text' => 'Efter lite detektivarbete har vi upptäckt att innehållet som du anmälde inte går mot våra riktlinjer. Om du vill att vi ska granska något specifikt kan du anmäla utseendet, profilen etc och ger oss ytterligare kontext när det behövs. Tack för din involvering och oro i våran community!',
-        ]
-    ]
+        ],
+    ],
 ];
