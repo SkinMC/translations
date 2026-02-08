5c58117673a699a3836c1071713412ba0979d542
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-08 15:59:56 +0100

    New translations alerts.php (Persian)

--- fa/alerts.php
+++ fa/alerts.php
@@ -11,21 +11,21 @@ return [
     */
     'follower' => [
         'title' => 'دنبال کننده جدید',
-        'text' => ':user دارد شما را دنبال می کند!'
+        'text' => ':user دارد شما را دنبال می کند!',
     ],
     'comment' => [
         'new' => [
             'title' => 'نظر مدل',
-            'text' => ':user یک نظر روی مدلتان درست کرد'
+            'text' => ':user یک نظر روی مدلتان درست کرد',
         ],
         'reply' => [
             'title' => 'پاسخ به نظر مدل',
-            'text' => 'کاربر به نظر مدل پاسخ داد'
+            'text' => 'کاربر به نظر مدل پاسخ داد',
         ],
         'like' => [
             'title' => 'Comment liked',
-            'text' => ':user liked your comment!'
-        ]
+            'text' => ':user liked your comment!',
+        ],
     ],
     'post' => [
         'like' => [
@@ -34,23 +34,29 @@ return [
         ],
         'mention' => [
             'title' => 'اشاره جدید',
-            'text' => ':user به شما در پستشان اشاره کرد!'
+            'text' => ':user به شما در پستشان اشاره کرد!',
         ],
         'repost' => [
             'title' => 'Repost',
-            'text' => ':user reposted your post!'
-        ]
+            'text' => ':user reposted your post!',
+        ],
     ],
     'level' => [
         'up' => [
             'title' => 'You leveled up!',
-            'text' => 'Congratulations on reaching level :level!'
-        ]
+            'text' => 'Congratulations on reaching level :level!',
+        ],
+    ],
+    'board' => [
+        'collaborator_added' => [
+            'title' => 'Added to a board',
+            'text' => ':user added you as a collaborator to the board ":board".',
+        ],
     ],
     'report' => [
         'created' => [
             'title' => 'We\'ve received your report! (Case :id)',
-            'text' => 'Our team is on it and will look into it right away. Thanks for being awesome and helping us keep things chill! '
+            'text' => 'Our team is on it and will look into it right away. Thanks for being awesome and helping us keep things chill! ',
         ],
         'resolved' => [
             'title' => 'We reviewed your report (Case :id)',
@@ -59,6 +65,6 @@ return [
         'dismissed' => [
             'title' => 'Update to your report (Case :id)',
             'text' => 'After some detective work, we\'ve found that the content you had reported doesn\'t go against our guidelines. If you want us to review something specific, please make sure that you report the skin, profile, etc, and provide additional context when needed. Thanks for your concern and involvement in our community!',
-        ]
-    ]
+        ],
+    ],
 ];
