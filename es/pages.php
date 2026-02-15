2c0a8585d779cdcdaa0bdc2d1e31158c9eda1753
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-16 00:34:58 +0100

    New translations pages.php (Spanish)

--- es/pages.php
+++ es/pages.php
@@ -48,7 +48,7 @@ return [
         'servers_browse' => 'Navegar servidores',
         'servers' => 'Descubre los mejores servidores de Minecraft',
         'servers_new_upcoming' => 'New & upcoming servers',
-        'servers_new_upcoming_browse' => 'Find fresh servers with smaller communities that are currently online.',
+        'servers_new_upcoming_browse' => 'Find fresh servers with smaller communities that are online and showing promising activity.',
         'servers_upcoming_players' => 'players',
         'servers_upcoming_badge' => 'New',
         'heads' => 'Cabezas de Minecraft',
