6d466fdb112468432c234f0eb08f29921598f220
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-15 13:56:29 +0100

    New translations xp.php (Portuguese, Brazilian)

--- pt/xp.php
+++ pt/xp.php
@@ -20,6 +20,11 @@ return [
         'sign_up' => 'Bem-vindo(a) ao SkinMC.',
         'invite_friend' => 'Convidar amigo',
         'invited_friend' => 'Convidado por um amigo',
+        'chat_streak_milestone_3' => 'Unlocked a 3-day chat streak',
+        'chat_streak_milestone_10' => 'Unlocked a 10-day chat streak',
+        'chat_streak_milestone_30' => 'Unlocked a 30-day chat streak',
+        'chat_streak_milestone_100' => 'Unlocked a 100-day chat streak',
+        'chat_streak_milestone_200' => 'Unlocked a 200-day chat streak',
     ],
     'empty' => 'Obtenha mais EXP para ver aqui.',
 ];
