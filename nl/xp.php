ee09f55ef8bfb3fd9cd6e704125bd3d782afec22
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-15 13:56:22 +0100

    New translations xp.php (Dutch)

--- nl/xp.php
+++ nl/xp.php
@@ -20,6 +20,11 @@ return [
         'sign_up' => 'Welkom bij SkinMC.',
         'invite_friend' => 'Een vriend uitnodigen',
         'invited_friend' => 'Uitgenodigd door een vriend',
+        'chat_streak_milestone_3' => 'Unlocked a 3-day chat streak',
+        'chat_streak_milestone_10' => 'Unlocked a 10-day chat streak',
+        'chat_streak_milestone_30' => 'Unlocked a 30-day chat streak',
+        'chat_streak_milestone_100' => 'Unlocked a 100-day chat streak',
+        'chat_streak_milestone_200' => 'Unlocked a 200-day chat streak',
     ],
     'empty' => 'Verdien wat XP om het hier te zien.',
 ];
