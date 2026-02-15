cb78c92ffda548d34d163400a4118b7b715f12e2
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-15 13:56:20 +0100

    New translations xp.php (Japanese)

--- ja/xp.php
+++ ja/xp.php
@@ -20,6 +20,11 @@ return [
         'sign_up' => 'SkinMCへようこそ',
         'invite_friend' => '友だちを招待する',
         'invited_friend' => '友人からの招待',
+        'chat_streak_milestone_3' => 'Unlocked a 3-day chat streak',
+        'chat_streak_milestone_10' => 'Unlocked a 10-day chat streak',
+        'chat_streak_milestone_30' => 'Unlocked a 30-day chat streak',
+        'chat_streak_milestone_100' => 'Unlocked a 100-day chat streak',
+        'chat_streak_milestone_200' => 'Unlocked a 200-day chat streak',
     ],
     'empty' => 'XPを稼げばそれを見ることが出来ます。',
 ];
