be5e1847a344f7df8c3683b39eeeb326ce85b6f1
Zane <62026364+zanethefox@users.noreply.github.com>
2026-02-16 00:34:59 +0100

    New translations pages.php (Arabic)

--- ar/pages.php
+++ ar/pages.php
@@ -48,7 +48,7 @@ return [
         'servers_browse' => 'استعراض السيرفرات',
         'servers' => 'اكتشف أفضل خوادم ماين كرافت',
         'servers_new_upcoming' => 'New & upcoming servers',
-        'servers_new_upcoming_browse' => 'Find fresh servers with smaller communities that are currently online.',
+        'servers_new_upcoming_browse' => 'Find fresh servers with smaller communities that are online and showing promising activity.',
         'servers_upcoming_players' => 'players',
         'servers_upcoming_badge' => 'New',
         'heads' => 'رؤوس ماينكرافت',
