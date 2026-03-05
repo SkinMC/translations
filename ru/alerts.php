7998ed1a7fb4d70ca56cfe8faafdc674ba2dfc83
Zane <62026364+zanethefox@users.noreply.github.com>
2026-03-05 13:22:17 +0100

    New translations alerts.php (Russian)

--- ru/alerts.php
+++ ru/alerts.php
@@ -45,6 +45,12 @@ return [
             'text' => ':user перепостил ваш пост!',
         ],
     ],
+    'chat' => [
+        'new_message' => [
+            'title' => 'New message',
+            'text' => ':user sent you a message.',
+        ],
+    ],
     'level' => [
         'up' => [
             'title' => 'Ваш уровень повышен!',
