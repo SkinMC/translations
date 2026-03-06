48642a69523d3213e8c5267b1154ea02f8d16771
Zane <62026364+zanethefox@users.noreply.github.com>
2026-03-06 18:36:38 +0100

    New translations alerts.php (Italian)

--- it/alerts.php
+++ it/alerts.php
@@ -77,6 +77,12 @@ return [
             'text' => 'Dopo qualche indagine, abbiamo scoperto che il contenuto che hai segnalato non va contro le nostre linee guida. Se vuoi farci visionare qualcosa di specifico, assicurati di segnalare la skin, il profilo, ecc, e di fornire un contesto aggiuntivo quando necessario. Grazie per la tua sollecitazione e il tuo aiuto alla nostra comunità!',
         ],
     ],
+    'system' => [
+        'discord_invite' => [
+            'title' => 'Join the discord',
+            'text' => 'Hi there, you\'re invited to join the SkinMC Discord server.',
+        ],
+    ],
     'server' => [
         'review_received' => [
             'title' => 'Nuova recensione su :server',
