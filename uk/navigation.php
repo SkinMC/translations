20105dab74e950abe93289a598ef0f94137699d9
Zane <62026364+zanethefox@users.noreply.github.com>
2026-03-10 05:19:55 +0100

    New translations navigation.php (Ukrainian)

--- uk/navigation.php
+++ uk/navigation.php
@@ -19,6 +19,7 @@ return [
     'banner' => 'Banner Editor',
     'banner_gallery' => 'Banner Gallery',
     'profile' => 'Мій профіль',
+    'wallpapers' => 'Wallpapers',
 
     'generators' => 'Генератори',
 
