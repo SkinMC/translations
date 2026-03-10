a48c7ee9730df8831424d94f12142dbbc083cc49
Zane <62026364+zanethefox@users.noreply.github.com>
2026-03-10 17:40:45 +0100

    New translations xp.php (Swedish)

--- sv/xp.php
+++ sv/xp.php
@@ -20,6 +20,8 @@ return [
         'sign_up' => 'Välkommen till SkinMC.',
         'invite_friend' => 'Bjud in en vän',
         'invited_friend' => 'Inbjuden av en vän',
+        'wallpaper_create' => 'Submitted a wallpaper',
+        'wallpaper_approved' => 'Wallpaper was approved',
         'chat_streak_milestone_3' => 'Unlocked a 3-day chat streak',
         'chat_streak_milestone_10' => 'Unlocked a 10-day chat streak',
         'chat_streak_milestone_30' => 'Unlocked a 30-day chat streak',
