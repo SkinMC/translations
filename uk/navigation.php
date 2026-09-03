a896cf3e26fcd56f3050eaa785fc6b15d8febe9e
Zane <62026364+zanethefox@users.noreply.github.com>
2026-09-03 15:02:43 +0200

    New translations navigation.php (Ukrainian)
    [ci skip]

--- uk/navigation.php
+++ uk/navigation.php
@@ -18,6 +18,7 @@ return [
     'banners' => 'Стяги',
     'banner' => 'Banner Editor',
     'banner_gallery' => 'Banner Gallery',
+    'cape_editor' => 'Cape Editor',
     'profile' => 'Мій профіль',
     'wallpapers' => 'Wallpapers',
 
