33f0dd672fad5facb6affa53e274a505ec3c92e1
Zane <62026364+zanethefox@users.noreply.github.com>
2026-09-03 15:02:04 +0200

    New translations navigation.php (Hebrew)
    [ci skip]

--- he/navigation.php
+++ he/navigation.php
@@ -18,6 +18,7 @@ return [
     'banners' => 'באנרים',
     'banner' => 'Banner Editor',
     'banner_gallery' => 'Banner Gallery',
+    'cape_editor' => 'Cape Editor',
     'profile' => 'הפרופיל שלי',
     'wallpapers' => 'Wallpapers',
 
