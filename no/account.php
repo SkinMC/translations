2417d41e03dbe63d7596337a5dba7401a302b309
Zane <62026364+zanethefox@users.noreply.github.com>
2026-09-14 15:40:02 +0200

    New translations account.php (Norwegian)
    [ci skip]

---
+++ no/account.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'security' => [
+        'title' => 'Security and safety',
+        'description' => 'Manage your login, message permissions and blocked accounts.',
+        'messaging' => 'Messages and invitations',
+    ],
+];
